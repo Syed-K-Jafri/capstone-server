737c1579d15259f7ded176bac613b375d1ef35a6
Syed-K-Jafri <syedjafri1@yahoo.com>
2020-12-22 20:29:07 +0500

    user controller

---
+++ controllers/users_controller.js
@@ -0,0 +1,14 @@
+const userModel = require ('../models/users_model');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+const db = require('../config/db_config');
+
+class Users {
+  
+    constructor() { }
+
+
+}
+
+module.exports = new Users();
