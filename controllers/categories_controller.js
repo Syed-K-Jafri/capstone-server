9c37c3da08727990355849b23d74657b9d151fef
Syed-K-Jafri <syedjafri1@yahoo.com>
2020-12-22 20:32:53 +0500

    category controller

---
+++ controllers/categories_controller.js
@@ -0,0 +1,14 @@
+const categoriesModel = require('../models/categories_model');
+const questionModel = require('../models/questions_model');
+const userModel = require('../models/users_model');
+const answerModel = require('../models/answers_model');
+const db = require('../config/db_config');
+
+class Categories {
+  
+    constructor() { }
+
+
+}
+
+module.exports = new Categories();
