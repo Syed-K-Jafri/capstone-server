a972946a786d4fa3a78667859869555e1fdda1c2
Syed-K-Jafri <syedjafri1@yahoo.com>
2020-12-22 20:33:27 +0500

    create category

--- controllers/categories_controller.js
+++ controllers/categories_controller.js
@@ -8,7 +8,29 @@ class Categories {
   
     constructor() { }
 
-
+    createCategory() {
+        return async (req, res) => { 
+         
+            const { title } = req.body;
+            
+            if (!req.body || !title) {
+                return res.status(400).send({ msg: 'Bad Request' });
+            }
+            try {
+                const category = await categoriesModel.findOne({ where: { title: title } });
+                if (category) {
+                    return res.status(409).send({ msg: 'Category against this title already exist.' });
+                } else {
+                    const newCategory = await categoriesModel.create({ title });
+                    return res.status(200).json({ msg: 'Category Created Successfully' });
+                }
+            } catch (err) {
+                console.log('Error in finding category: ', err);
+                return res.status(500).json({ msg: 'Internal Server Error', error: err });
+            }
+        }
+    }
+    
 }
 
 module.exports = new Categories();
