506c11efb02ba381d5d03f5eb862451117aefbee
Syed-K-Jafri <syedjafri1@yahoo.com>
2020-12-22 20:34:12 +0500

    delete category

--- controllers/categories_controller.js
+++ controllers/categories_controller.js
@@ -31,6 +31,23 @@ class Categories {
         }
     }
     
+    deleteCategories() {
+        return async (req, res) => { 
+            const { category_id } = req.body;
+            
+            if (!req.body || !category_id) {
+                return res.status(400).send({ msg: 'Bad Request' });
+            }
+
+            try {
+                const result = await categoriesModel.update({ is_deleted: true }, { where: { id: category_id } });
+                return res.status(200).send({ msg: 'Category Deleted Successfully' });
+            } catch (err) {
+                console.log('Error in deleting specific category from db', err);
+                return res.status(500).json({ msg: 'Internal Server Error', error: err });
+            }
+        }
+    }
 }
 
 module.exports = new Categories();
